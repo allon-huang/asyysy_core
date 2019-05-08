dfe96f89b48c9979f2109b3ebc871c5cb1cd8b72
hyy <hyy210>
2019-05-08 23:26:10 +0800

    *) 微信拦截器配置

--- src/main/java/cn/asyysy/core/common/config/WebConfigurer.java
+++ src/main/java/cn/asyysy/core/common/config/WebConfigurer.java
@@ -34,7 +34,7 @@ public class WebConfigurer implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // addPathPatterns 需要拦截的路径/** 为全部    excludePathPatterns 拦截路径下排除的路径
-        registry.addInterceptor(loginIntercepter).addPathPatterns("/**").excludePathPatterns(Arrays.asList("/user/login", "/index"));
+        registry.addInterceptor(loginIntercepter).addPathPatterns("/**").excludePathPatterns(Arrays.asList("/user/login", "/index","/mp/*"));
         // 注册Token拦截器
         //registry.addInterceptor(new ApiSignatureInterceptor());
         //registry.addInterceptor(new JwtInterceptor()) .addPathPatterns("/tp_store/logout");
